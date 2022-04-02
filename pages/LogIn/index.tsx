d4fee2b63f4f6b0d70abee6068ef3627b4de03cb
hyeri <rhkk7157@gmail.com>
2022-04-03 01:14:46 +0900

    로그인

--- pages/LogIn/index.tsx
+++ pages/LogIn/index.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import useInput from '@hooks/useInput';
+import { Button, Form, Header, Input, Label, LinkContainer } from '@pages/SignUp/style';
+import React, { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LogIn = () => {
-  return <div>로그인</div>;
+  const [logInError, setLogInError] = useState(false);
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
 };
 
 export default LogIn;
